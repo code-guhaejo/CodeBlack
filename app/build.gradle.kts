c7d32b8b110d597fe5a13ac089f62106d9ec5e81
Yugyeong Park <bonbonly7@naver.com>
2024-05-09 19:36:51 +0900

    retrofit2 의존성 중복 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation ("com.squareup.okhttp3:okhttp:4.9.1")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.8.0")
     implementation ("com.squareup.okhttp3:okhttp-urlconnection:4.9.1")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
