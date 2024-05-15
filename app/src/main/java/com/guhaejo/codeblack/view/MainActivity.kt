ffca31bd9fdc7b09eeaae3edf27d9037508dd406
Yugyeong Park <bonbonly7@naver.com>
2024-05-15 20:02:22 +0900

    MainActivity의 Google 로그인 버튼 설정 추가 로그인 버튼이 클릭되면, LoginGoogleActivity로 이동

--- app/src/main/java/com/guhaejo/codeblack/view/MainActivity.kt
+++ app/src/main/java/com/guhaejo/codeblack/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.gms.common.SignInButton
 import com.guhaejo.codeblack.BottomNavActivity
 import com.guhaejo.codeblack.R
 
@@ -16,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         loginBtn.setOnClickListener {
             val intent = Intent(this, BottomNavActivity::class.java)
             startActivity(intent)
+        }
+
+        // Google 로그인 버튼 설정
+        val googleLoginBtn: SignInButton = findViewById(R.id.sign_in_button)
+        googleLoginBtn.setOnClickListener {
+            val googleLoginIntent = Intent(this, LoginGoogleActivity::class.java)
+            startActivity(googleLoginIntent)
         }
     }
 }
