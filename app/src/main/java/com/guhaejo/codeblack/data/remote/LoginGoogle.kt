aaa6a262e3d936e9ee89621b4cb12ae8112c65ad
Yugyeong Park <bonbonly7@naver.com>
2024-05-16 18:28:17 +0900

    - 코루틴을 사용하여 비동기 작업을 처리, 네트워크 요청을 처리 - lifecycleScope를 사용하여 생명 주기를 따르도록 변경

--- app/src/main/java/com/guhaejo/codeblack/data/remote/LoginGoogle.kt
+++ app/src/main/java/com/guhaejo/codeblack/data/remote/LoginGoogle.kt
@@ -5,14 +5,22 @@ import android.content.Intent
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.guhaejo.codeblack.repository.LoginRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
-class LoginGoogle(context: Context) {
+class LoginGoogle(private val context: Context, private val lifecycleOwner: LifecycleOwner) {
     // 구글 로그인 옵션 설정
     private val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
         .requestIdToken(ClientInformation.CLIENT_ID)
@@ -25,9 +33,19 @@ class LoginGoogle(context: Context) {
     // 로그인 결과 처리
     fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
-            val authCode: String? =
-                completedTask.getResult(ApiException::class.java)?.serverAuthCode // 서버 인증 코드
-            LoginRepository().getAccessToken(authCode!!) // 액세스 토큰 요청
+//            val authCode: String? =
+//                completedTask.getResult(ApiException::class.java)?.serverAuthCode // 서버 인증 코드
+//            LoginRepository().getAccessToken(authCode!!) // 액세스 토큰 요청
+
+            val account = completedTask.getResult(ApiException::class.java)
+            val idToken = account?.idToken
+            if (idToken != null) {
+                lifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+                    sendAccessTokenToServer(idToken)
+                }
+            } else {
+                Log.w(TAG, "ID Token is null")
+            }
         } catch (e: ApiException) {
             Log.w(TAG, "handleSignInResult: error" + e.statusCode)
         }
@@ -56,7 +74,36 @@ class LoginGoogle(context: Context) {
     // 로그인 상태 확인
     fun isLogin(context: Context): Boolean {
         val account = GoogleSignIn.getLastSignedInAccount(context) // 마지막 로그인 계정을 가져온다
-        return if (account == null) false else (true) // 계정이 null인지 확인, 반환
+        return account != null
+        // return if (account == null) false else (true) // 계정이 null인지 확인, 반환
+    }
+
+    private suspend fun sendAccessTokenToServer(accessToken: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val url = URL("http://10.0.2.2:8080/token")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.readTimeout = 15000
+                connection.connectTimeout = 15000
+                connection.doOutput = true
+
+                val outputStream = DataOutputStream(connection.outputStream)
+                outputStream.writeBytes("access_token=$accessToken")
+                outputStream.flush()
+                outputStream.close()
+
+                val responseCode = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val response = connection.inputStream.bufferedReader().use { it.readText() }
+                    Log.d(TAG, "Server response: $response")
+                } else {
+                    Log.e(TAG, "Server error: $responseCode")
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "sendOnFailure: ${e.message}")
+            }
+        }
     }
 
     companion object {
