6f1a57c61e09a6e400798b1eb4dab84484e3b969
kookyungseon <114800024+kookyungseon@users.noreply.github.com>
2024-05-17 13:02:26 +0900

    구글로 연동하기 custom bottom으로 변경

---
+++ app/src/main/java/com/guhaejo/codeblack/data/remote/google/ClientInformation.kt
@@ -0,0 +1,6 @@
+package com.guhaejo.codeblack.data.remote.google
+
+object ClientInformation {
+    const val CLIENT_ID = "30120983380-5s0ss98r246p32vks06p4e4avqb1rcl9.apps.googleusercontent.com"
+    const val CLIENT_SECRET = "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"
+}
