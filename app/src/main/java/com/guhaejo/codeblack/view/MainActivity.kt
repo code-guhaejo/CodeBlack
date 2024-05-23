b37e415ab13840f8af332bbace754e16d03c0565
Yugyeong Park <bonbonly7@naver.com>
2024-05-24 00:25:53 +0900

    로그인에 대한 클릭 리스너를 모두 LoginActivity로 이동

--- app/src/main/java/com/guhaejo/codeblack/view/MainActivity.kt
+++ app/src/main/java/com/guhaejo/codeblack/view/MainActivity.kt
@@ -4,26 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.google.android.gms.common.SignInButton
 import com.guhaejo.codeblack.BottomNavActivity
 import com.guhaejo.codeblack.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-        val loginBtn: Button = findViewById(R.id.login_btn)
-        loginBtn.setOnClickListener {
-            val intent = Intent(this, BottomNavActivity::class.java)
-            startActivity(intent)
-        }
-
-        // Google 로그인 버튼 설정
-        val googleLoginBtn: SignInButton = findViewById(R.id.sign_in_button)
-        googleLoginBtn.setOnClickListener {
-            val googleLoginIntent = Intent(this, LoginGoogleActivity::class.java)
-            startActivity(googleLoginIntent)
-        }
+//        setContentView(R.layout.activity_login)
+//
+//        val loginBtn: Button = findViewById(R.id.login_btn)
+//        loginBtn.setOnClickListener {
+//            val intent = Intent(this, BottomNavActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
