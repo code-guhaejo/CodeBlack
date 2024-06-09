00111c20508fe6d0026b9fa1673bdd8a6e3b15b0
Yugyeong Park <bonbonly7@naver.com>
2024-06-09 18:53:55 +0900

    불필요한 SerializedName 어노테이션 삭제

--- app/src/main/java/com/guhaejo/codeblack/data/remote/loginlocal/model/SignInResponse.kt
+++ app/src/main/java/com/guhaejo/codeblack/data/remote/loginlocal/model/SignInResponse.kt
@@ -3,8 +3,6 @@ package com.guhaejo.codeblack.data.remote.loginlocal.model
 import com.google.gson.annotations.SerializedName
 
 data class SignInResponse(
-    @SerializedName("success")
     val success: Boolean,
-    @SerializedName("message")
     val message: String
 )
