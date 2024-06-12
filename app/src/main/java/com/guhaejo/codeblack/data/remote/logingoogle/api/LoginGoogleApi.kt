5ae40636407fdbe0aecf3eab876f34e552ead63e
Yugyeong Park <bonbonly7@naver.com>
2024-06-12 20:04:22 +0900

    LoginGoogleService에서 LoginGoogleApi로 이름 변경

--- app/src/main/java/com/guhaejo/codeblack/data/remote/logingoogle/api/LoginGoogleApi.kt
+++ app/src/main/java/com/guhaejo/codeblack/data/remote/logingoogle/api/LoginGoogleApi.kt
@@ -12,7 +12,7 @@ import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
-interface LoginGoogleService {
+interface LoginGoogleApi {
     // 구글 OAuth 2.0 토큰 요청
     @POST("oauth2/v4/token")
     fun getAccessToken(
@@ -24,19 +24,19 @@ interface LoginGoogleService {
     @Headers("content-type: application/json")
     fun sendAccessToken(
         @Body accessToken: SendAccessTokenModel
-    ):Call<String>
+    ): Call<String>
 
     companion object {
         private val gson = GsonBuilder().setLenient().create()
 
-        // Retrofit 인스턴스 생성 후 LoginGoogleService 반환
-        fun loginRetrofit(baseUrl: String): LoginGoogleService {
+        // Retrofit 인스턴스 생성 후 LoginGoogleApi 반환
+        fun loginRetrofit(baseUrl: String): LoginGoogleApi {
             return Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
-                .create(LoginGoogleService::class.java)
+                .create(LoginGoogleApi::class.java)
         }
     }
 }
