294eec1c590b6d8f5109c758f95d56c0b53790a6
Yugyeong Park <bonbonly7@naver.com>
2024-06-14 02:22:50 +0900

    GoogleSignInClient 추가

--- app/src/main/java/com/guhaejo/codeblack/data/remote/logingoogle/LoginGoogle.kt
+++ app/src/main/java/com/guhaejo/codeblack/data/remote/logingoogle/LoginGoogle.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
